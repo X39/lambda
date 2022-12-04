e9667a9b9cc6150ce9e7379ed0e81b1c83895d88
X39 <marco@silipo.de>
2022-12-04 23:47:06 +0100

    state

--- src/machine/runner.rs
+++ src/machine/runner.rs
@@ -1,9 +1,10 @@
 use std::borrow::{Borrow, BorrowMut};
-use crate::machine::{Instruction, InstructionArg, OpCode, VmValue};
+use crate::machine::{Instruction, InstructionArg, OpCode, VmPair, VmValue};
 use crate::VmState;
 
 pub struct VmStack {
     data: Vec<VmValue>,
+    variables: Vec<VmPair>,
 }
 
 impl VmState {
@@ -35,41 +36,52 @@ impl VmState {
                     InstructionArg::Signed(_) => { return Err("PushValueU16 argument is signed."); }
                     InstructionArg::Type(_) => { return Err("PushValueU16 argument is type."); }
                 }
-            },
+            }
             OpCode::PushTrue => {
                 stack.data.push(VmValue::Boolean(true));
-            },
+            }
             OpCode::PushFalse => {
                 stack.data.push(VmValue::Boolean(false));
-            },
+            }
             OpCode::PushNull => {
                 stack.data.push(VmValue::Null);
-            },
+            }
             OpCode::PushEmptyArray => {
                 stack.data.push(VmValue::Array(vec![]));
-            },
+            }
             OpCode::PushEmptyObject => {
                 stack.data.push(VmValue::Object(vec![]));
-            },
-            OpCode::GetVariable => {},
-            OpCode::GetVariableOfType => {},
-            OpCode::AppendArrayPush => {},
-            OpCode::AppendPropertyPush => {},
-            OpCode::Assign => {},
-            OpCode::Pop => {},
-            OpCode::Jump => {},
-            OpCode::JumpIfFalse => {},
-            OpCode::JumpIfTrue => {},
-            OpCode::JumpIterate => {},
-            OpCode::Swap2 => {},
+            }
+            OpCode::GetVariable => {
+                let var_name_opt = stack.data.pop();
+                if var_name_opt.is_none() {
+                    return Err("GetVariable could not pop a STRING value from an empty stack");
+                }
+                match var_name_opt.unwrap() {
+                    VmValue::String(s) => {
+                        // ToDo
+                    },
+                    _ => return Err("GetVariable expected STRING value on stack"),
+                }
+            }
+            OpCode::GetVariableOfType => {}
+            OpCode::AppendArrayPush => {}
+            OpCode::AppendPropertyPush => {}
+            OpCode::Assign => {}
+            OpCode::Pop => {}
+            OpCode::Jump => {}
+            OpCode::JumpIfFalse => {}
+            OpCode::JumpIfTrue => {}
+            OpCode::JumpIterate => {}
+            OpCode::Swap2 => {}
 
-            OpCode::Await => {},
-            OpCode::Abort => {},
-            OpCode::AbortAll => {},
-            OpCode::AwaitAny => {},
-            OpCode::AwaitAll => {},
-            OpCode::Call => {},
-            OpCode::CallNoArg => {},
+            OpCode::Await => {}
+            OpCode::Abort => {}
+            OpCode::AbortAll => {}
+            OpCode::AwaitAny => {}
+            OpCode::AwaitAll => {}
+            OpCode::Call => {}
+            OpCode::CallNoArg => {}
         };
         Ok(())
     }
